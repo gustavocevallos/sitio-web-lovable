c3814c0a1bd82fe2a7eccb69fc911d1cd62f1ac2
Gustavo Cevallos <gustavocevallospulley@gmail.com>
2026-05-25 22:43:33 +0200

    Change Landbot Container to Livechat
    Updated the Landbot integration to use Livechat instead of Container.

--- public/landbot-loader.js
+++ public/landbot-loader.js
@@ -1,22 +1,13 @@
 (function () {
-  // 1. Crear el contenedor del chatbot e insertarlo al final del cuerpo de la web
-  const div = document.createElement('div');
-  div.id = 'myLandbot';
-  div.style.width = '100%';
-  div.style.height = '500px';
-  div.style.minHeight = '500px';
-  div.style.backgroundColor = 'transparent';
-  document.body.appendChild(div);
-
-  // 2. Cargar el script de Landbot
+  // 1. Cargamos el script estándar de Landbot
   const script = document.createElement('script');
   script.src = 'https://cdn.landbot.io/landbot-3/landbot-3.0.0.js';
   script.async = true;
 
   script.onload = function () {
-    if (window.Landbot && window.Landbot.Container) {
-      new window.Landbot.Container({
-        container: '#myLandbot',
+    if (window.Landbot) {
+      // 2. IMPORTANTE: Cambiamos "Container" por "Livechat" para que sea flotante
+      new window.Landbot.Livechat({
         configUrl: 'https://storage.googleapis.com/landbot.online/v3/H-3412687-R7QF0JBGNGWE21NU/index.json',
       });
     }
